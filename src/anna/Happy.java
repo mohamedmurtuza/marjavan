c9a9ff2b63ba604951e4bd5aeffb74a176fbc092
mohamedmurtuza <mohamedmurtuza3@gmail.com>
2020-05-03 21:26:19 +0530

    Bas re ba bas

--- src/anna/Happy.java
+++ src/anna/Happy.java
@@ -6,6 +6,18 @@ public class Happy {
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
+		
+		System.out.println("1 2 3 ");
+		hell();
+		
 	}
+	
+	
+	public static void hell() {
+		
+		
+		System.out.println("Hey");
+	}
+	
 
 }
